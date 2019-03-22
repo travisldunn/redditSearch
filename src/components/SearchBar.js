a5998bb3e9f269655acbbe8b0380b177a24a737f
Travis Dunn <Travis.Dunn@macys.com>
2019-03-22 15:46:06 -0700

    get search to work

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const SearchBar = ({}) => {
-  return <h1>searchBar</h1>;
+const SearchBar = ({ onChange, onSubmit }) => {
+  return (
+    <div className="search">
+      <form onSubmit={(e => e.preventDefault(), onSubmit)}>
+        <input
+          type="text"
+          name="query"
+          className="searchBar"
+          placeholder="Search for a subreddit..."
+          onChange={e => onChange(e.currentTarget.value)}
+        />
+        <input className="button" value="Search" type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/main/RedditContainer.js
+++ src/main/RedditContainer.js
@@ -3,16 +3,34 @@ import SearchBar from "../components/SearchBar";
 import PostContainer from "../components/PostContainer";
 
 class RedditContainer extends Component {
-  state = {};
+  state = {
+    searchUrl: "https://www.reddit.com/r/funny.json?limit=25",
+    posts: []
+  };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.fetchPosts(this.state.searchUrl);
+  }
 
   componentWillUnmount() {}
+
+  handleSearch = query => {
+    console.log(query);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("submiting");
+  };
+
+  fetchPosts(url) {
+    console.log(url);
+  }
 
   render() {
     return (
       <div className="home">
-        <SearchBar />
+        <SearchBar onChange={this.handleSearch} onSubmit={this.handleSubmit} />
         <PostContainer />
       </div>
     );
